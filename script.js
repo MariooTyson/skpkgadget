285fe8a0b3f36ac308f1285cdd89aa5573b46618
MariooTyson <105903487+MariooTyson@users.noreply.github.com>
2022-05-20 12:50:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+let Hp = [];
+
+let informasi = {
+    foto:"https://cdn.eraspace.com/pub/media/catalog/product/i/p/iphone_13_pro_max_silver_1.jpg",
+    harga:22000000,
+    kode:"BRG001",
+    stock: 10,
+    nama:"iphone 13 pro max",
+    rating:"5.0",
+}
+Hp.push(informasi);
+let informasi2 = {
+    foto:"http://p-id.ipricegroup.com/uploaded_6a60b762bd1ed9d7b15d6755dba3262f.jpg",
+    harga:50000000,
+    kode:"BRG002",
+    stock: 20,
+    nama:"samsung A7 lite",
+    rating:"5.0",
+}
+Hp.push(informasi2);
+let informasi3 = {
+    foto:"https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/09/09/2954333388.jpg",
+    harga:7000000,
+    kode:"BRG003",
+    stock: 100,
+    nama:"oppo reno 9",
+    rating:"5.0",
+}
+Hp.push(informasi3);
+let informasi4 = {
+foto:"https://i01.appmifile.com/webfile/globalimg/id/cms/10B4246A-5AB3-13F7-B7F7-082776A7B5E7.jpg",
+harga:5000000,
+kode:"BRG004",
+stock: 1000,
+nama:"xiaomi redmi note 11",
+rating:"5.0",
+
+}
+Hp.push(informasi4);
+let informasi5 = {
+    foto:"https://selular.id/wp-content/uploads/2020/11/Vivo-Y20s.jpg",
+    harga:6000000,
+    kode:"BRG005",
+    stock: 50,
+    nama:"vivo",
+    rating:"5.0",
+}
+Hp.push(informasi5);
+let informasi6 = {
+    foto:"https://www.oppo.com/content/dam/oppo/common/mkt/v2-2/a16/listpage/A16-productlist-blue-427x600-v2.png",
+    harga:6000000,
+    kode:"BRG006",
+    stock: 40,
+    nama:"oppo a16",
+    rating:"5.0", 
+}
+Hp.push(informasi6);
+let informasi7 = {
+    foto:"https://static.bmdstatic.com/pk/product/large/5ff6d3d53459d.jpg",
+    harga:8000000,
+    kode:"BRG007",
+    stock: 10,
+    nama:"samsung a12",
+    rating:"5.0", 
+}
+Hp.push(informasi7);
+let informasi8 = {
+    foto:"https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2020/07/14/855438397.jpg",
+    harga:4000000,
+    kode:"BRG008",
+    stock: 11,
+    nama:"huawei nova 7",
+    rating:"4.0", 
+}
+Hp.push(informasi8);
+let informasi9 = {
+    foto:"https://cdn0-production-images-kly.akamaized.net/vRoekiOAYzcbOwUw10fhbY2xq-M=/1200x900/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/794754/original/079138200_1421213419-Nokia_3310.jpg",
+    harga:1000000,
+    kode:"BRG009",
+    stock: 11,
+    nama:"nokia",
+    rating:"4.0", 
+}
+Hp.push(informasi9);
+let informasi10 = {
+    foto:"https://i01.appmifile.com/webfile/globalimg/id/cms/772C9AEB-BFCA-3E19-878F-1FFC2BE50993.jpg",
+    harga:1000000,
+    kode:"BRG0010",
+    stock: 100,
+    nama:"redmi note 8",
+    rating:"4.0", 
+}
+    Hp.push(informasi10);
+
+console.log(Hp)
+
+
